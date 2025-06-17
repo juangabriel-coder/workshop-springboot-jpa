13605e0e19c8c050bf050b8579d1e89b6724737d
Juan <juan.sousa@academico.ifpb.edu.br>
2025-06-17 17:09:15 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -8,6 +8,14 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+		
+		try {
+            Class.forName("org.h2.Driver");
+            System.out.println("Driver H2 carregado com sucesso!");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Erro: Driver H2 não encontrado no classpath.");
+            e.printStackTrace();
+        }
 	}
 
 }
